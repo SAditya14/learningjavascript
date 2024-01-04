824be670ef6e7a0971ee5769b7e194d928b74037
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-04 13:30:02 +0000

    rating system

---
+++ ifelse-ex3.js
@@ -0,0 +1,26 @@
+// build a rating system for pizzahut
+
+// 1 star - poor
+// 2 star - emm, ok
+// 3 star - average
+// 4 star - cool
+// 5 star - great
+// 6 star - awesome
+// 7 star - nailed it
+
+let Star = 3;
+StarinNum = parseInt(Star);
+
+if(StarinNum = 1){
+    console.log("Poor")
+}else if(StarinNum = 2){
+    console.log("eem,ok")
+}else if(StarinNum = 3){
+    console.log("avg")
+}else if(StarinNum = 4){
+    console.log("nice")
+}else if(StarinNum = 5){
+    console.log("Loved")
+}else{
+    console.log("Please enter correct input")
+}
