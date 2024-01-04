146edcd7765c23bab939b36f3218b53d997bfe49
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-04 13:46:20 +0000

    Rating System using Switch

---
+++ switch.js
@@ -0,0 +1,25 @@
+let Star = 3;
+StarinNum = parseInt(Star);
+
+switch(StarinNum){
+    case 1:
+        console.log("Poor");
+        break;
+
+    case 2:
+        console.log("emm,OK");
+        break;    
+    
+    case 3:
+        console.log("Average");
+        break;         
+
+    case 4:
+        console.log("Good");
+        break;         
+    case 5:
+        console.log("Excellent");
+    
+    default:
+        console.log("Pleae give correct Rating");    
+    }
