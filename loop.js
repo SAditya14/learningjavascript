8144166388f3043091a8883509e027a1e33eec50
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-06 19:46:47 +0000

    for loop

--- loop.js
+++ loop.js
@@ -1,10 +1,9 @@
-// let userScore = 0
-// for ( let userScore ; userScore<=10  ; userScore++) {
-//     console.log("your score is ${userScore}");
+let userScore = 0
+let username = "AKS"
+for (userScore ; userScore<=10  ; userScore = userScore +1) {
+     if (userScore == 5){
+          continue;
+        }
+     console.log(`your score is ${userScore}`);
     
-// }
-
-let userS = 0;
-for (let userS ; userS>=10 ; userS= userS +1){
-    console.log(`your score is ${userS}`);
 }
