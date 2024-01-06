5cc0b0eb1d3f851c3a4a0a2a56839ecf720ce9c9
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-06 18:16:33 +0000

    if else ex4

---
+++ ifelse-ex4.js
@@ -0,0 +1,21 @@
+const email = "sdkjn"
+
+if (email == "adiks3947@gmail.com"){
+    console.log("Enter password");
+}else{
+    console.log("Enter you mail first");
+}
+console.log(Boolean(email));
+
+//logic 2 for the same
+
+// if (email != "") {
+//     console.log("Enter password");
+    
+// }else{
+//     console.log("Enter yout mail");
+// }
+
+// we can convert email to boolean as well for generalised emails
+
+
---
+++ loop.js
@@ -0,0 +1,10 @@
+// let userScore = 0
+// for ( let userScore ; userScore<=10  ; userScore++) {
+//     console.log("your score is ${userScore}");
+    
+// }
+
+let userS = 0;
+for (let userS ; userS>=10 ; userS= userS +1){
+    console.log(`your score is ${userS}`);
+}
