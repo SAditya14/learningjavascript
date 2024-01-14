754a0af5b8bfe0d65636c2f00fb9bcb06e6ebc7c
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-14 08:38:25 +0000

    learning scope

---
+++ Scopes/scopes.js
@@ -0,0 +1,19 @@
+var teacher = "jonny";
+
+function fun(){
+    var teacher = "sanket";
+    console.log(teacher);
+
+}
+
+function gun (){
+     student = "aditya";
+    console.log(teacher);
+
+}
+
+
+fun();
+gun();
+
+console.log(`${teacher} , ${student}`);
